cac3e139d35129f25d3ca5a5c8b26d152d86e76c
Bartłomiej Mytko <bartlomiej.mytko@komputronik.pl>
2020-05-04 20:47:32 +0200

    Dependency injection demo

--- src/main/java/com/luv2code/springdemo/BaseballCoach.java
+++ src/main/java/com/luv2code/springdemo/BaseballCoach.java
@@ -2,8 +2,19 @@ package com.luv2code.springdemo;
 
 public class BaseballCoach implements Coach {
 
+    private FortuneService service;
+
+    public BaseballCoach(FortuneService service) {
+        this.service = service;
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Please spand 30 minutes on batting practise.";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return service.getFortune();
     }
 }
--- src/main/java/com/luv2code/springdemo/TrackCoach.java
+++ src/main/java/com/luv2code/springdemo/TrackCoach.java
@@ -2,8 +2,19 @@ package com.luv2code.springdemo;
 
 public class TrackCoach implements Coach {
 
+    private FortuneService service;
+
+    public TrackCoach(FortuneService service) {
+        this.service = service;
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Please run a hard 5k.";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return service.getFortune();
     }
 }

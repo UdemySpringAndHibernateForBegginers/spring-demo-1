a38e1b342fa511312fe516440f24109ac6c2dad8
Bartłomiej Mytko <bartlomiej.mytko@komputronik.pl>
2020-05-05 10:44:29 +0200

    Bean scopes demo

---
+++ src/main/java/com/luv2code/springdemo/BeanScopeDemoApp.java
@@ -0,0 +1,27 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeDemoApp {
+
+    public static void main(String[] args) {
+
+        //load the spring configuration file
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+
+        //retrieve bean from spring container
+        Coach alphaCoach = context.getBean("myCoach", Coach.class);
+        Coach betaCoach = context.getBean("myCoach", Coach.class);
+
+        // check if they are the same
+        boolean result = alphaCoach == betaCoach;
+
+        //print result
+        System.out.println("Pointing to the same object? -> " + result);
+        System.out.println("Memory location for alphaCoach" + alphaCoach);
+        System.out.println("Memory location for betaCoach" + betaCoach);
+
+        //close the context
+        context.close();
+    }
+}
